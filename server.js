947ef97eb942a41b4290a70908ae2e90b95faf1c
Gai lowvapong <0kaiyast0@gmail.com>
2017-09-17 01:35:46 +0700

    Init Heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require('expres');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
